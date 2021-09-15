efd38c16daec2a6dd0a593a553b988862a4a8117
Paweł Cybulski <p.cybulski@mohi.to>
2021-09-15 13:25:37 +0200

    fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Link } from "gatsby"
-import { RiHeart2Line } from "react-icons/ri"
+// import { Link } from "gatsby"
+// import { RiHeart2Line } from "react-icons/ri"
 
 const Footer = () => (
   <footer
     className="site-footer"
     sx={{
-      bg: "siteColor",
+      bg: "siteColor"
     }}
   >
     <div className="container">
