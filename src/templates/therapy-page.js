f5876b7f95e14246787559f0d324cd32ff45dde7
Paweł Cybulski <p.cybulski@mohi.to>
2021-11-05 08:21:39 +0100

    about therapy page

--- src/templates/therapy-page.js
+++ src/templates/therapy-page.js
@@ -26,6 +26,29 @@ export const pageQuery = graphql`
 						aboutText
 					}
 				}
+				helpLinks {
+					helpLink {
+						helpLinkText
+						helpHref
+					}
+				}
+        therapyTypes {
+          therapyType {
+            therapyTypeTitle
+            therapyTypeSubtitle
+            therapyTypeId
+            therapyTypeImage {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+            therapyTypeList {
+              therapyListItem {
+                therapyListText
+              }
+            }
+          }
+        }
       }
     }
   }
@@ -36,65 +59,101 @@ const TherapyPage = ({ data }) => {
 	const heroImage = frontmatter.heroImage
 	? frontmatter.heroImage.childImageSharp.gatsbyImageData
 	: ""
-
   return (
     <Layout className="therapy">
       <Seo title={frontmatter.title} description={excerpt} />
-				<div class="therapy_header grids col-1 sm-2">
-					<div class="therapy_header--left">
-						<h1>
-							{frontmatter.title}
-							<br/>
-							<small>{frontmatter.subtitle}</small>
-						</h1>
-						<div class="therapy_blob">
+			<div class="therapy_header grids col-1 sm-2">
+				<div class="therapy_header--left">
+					<h1>
+						{frontmatter.title}
+						<br/>
+						<small>{frontmatter.subtitle}</small>
+					</h1>
+					<div class="therapy_blob">
+						<div class="tk-blob">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 274 303.2">
+								<path d="M260.2 41.4c20 29.2 14.6 74.5 7.2 124.4-7.3 49.9-16.6 104.5-49.2 126-32.5 21.6-88.4 10.2-132-15.2s-75-64.7-83.6-107.8C-6.1 125.7 8 79 36.3 47.8 64.5 16.7 107 1.3 150.9.1c43.9-1.1 89.3 12 109.3 41.3z"></path>
+							</svg>
+						</div>
+						<p>{frontmatter.howItWorks}</p>
+					</div>
+				</div>
+				<div class="therapy_header--right">
+					{heroImage ? (
+						<GatsbyImage
+							image={heroImage}
+							alt={frontmatter.title + " - Featured image"}
+							className="featured-image"
+						/>
+					) : (
+						""
+					)}
+				</div>
+			</div>
+			<div class="therapy_about-list">
+				<ol>
+					{frontmatter.aboutTherapy.map(({ aboutItem, index }) => (
+						<li key={index}>
+							<h3>{aboutItem.aboutHeading}</h3>
+							<p>{aboutItem.aboutText}</p>
+						</li>
+					))}
+				</ol>
+			</div>
+			<div class="home-how">
+				<div class="grids col-1 sm-2">
+					<h2>Jak mogę <span class="underline">ci pomóc?</span></h2>
+				</div>
+				<ul class="grids col-1 sm-3">
+					{frontmatter.helpLinks.map(({ helpLink, index }) => (
+						<li key={index}>
+							<a href={helpLink.helpHref}>{helpLink.helpLinkText}</a>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div class="therapy-types">
+				{frontmatter.therapyTypes.map(({ therapyType, index }) => (
+					<div key={index} id={therapyType.therapyTypeId}>
+						<div class="therapy-types_header">
 							<div class="tk-blob">
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 274 303.2">
 									<path d="M260.2 41.4c20 29.2 14.6 74.5 7.2 124.4-7.3 49.9-16.6 104.5-49.2 126-32.5 21.6-88.4 10.2-132-15.2s-75-64.7-83.6-107.8C-6.1 125.7 8 79 36.3 47.8 64.5 16.7 107 1.3 150.9.1c43.9-1.1 89.3 12 109.3 41.3z"></path>
 								</svg>
 							</div>
-							<p>{frontmatter.howItWorks}</p>
+							<h2>
+								{therapyType.therapyTypeTitle}
+								<p>{therapyType.therapyTypeSubtitle}</p>
+							</h2>
 						</div>
+						<div class="therapy-types_mid">
+							<p>
+								TERAPIA JEST
+								<span class="text-float-left">DLA</span>
+								<strong class="text-special">ciebie</strong>
+								<span class="text-float-right">GDY:</span>
+							</p>
+							{therapyType.therapyTypeImage ? (
+								<GatsbyImage
+									image={therapyType.therapyTypeImage.childImageSharp.gatsbyImageData}
+									alt="Featured image"
+									className="featured-image"
+								/>
+							) : (
+								""
+							)}
+						</div>
+						<ul class="therapy-types_list">
+							{therapyType.therapyTypeList.map(({ therapyListItem, index }) => (
+								<li key={index} class="therapy-types_list-item">
+									{therapyListItem.therapyListText}
+								</li>
+							))}
+						</ul>
 					</div>
-					<div class="therapy_header--right">
-						{heroImage ? (
-							<GatsbyImage
-								image={heroImage}
-								alt={frontmatter.title + " - Featured image"}
-								className="featured-image"
-							/>
-						) : (
-							""
-						)}
-					</div>
-				</div>
-				<div class="therapy_about-list">
-					<ol>
-						{frontmatter.aboutTherapy.map(({ aboutItem, index }) => (
-							<li key={index}>
-								<h3>{aboutItem.aboutHeading}</h3>
-								<p>{aboutItem.aboutText}</p>
-							</li>
-						))}
-					</ol>
-				</div>
-				<div class="home-how">
-				<div class="grids col-1 sm-2">
-					<h2>Jak mogę <span class="underline">ci pomóc?</span></h2>
-				</div>
-				<div class="grids col-1 sm-3">
-					<a href="#Relation">CHCĘ NAPRAWIĆ RELACJE</a>
-					<a href="#Depression">CIERPIĘ NA DEPRESJĘ</a>
-					<a href="#Stress">NIE RADZĘ SOBIE ZE STRESEM</a>
-				</div>
-				<div class="grids col-1 sm-3">
-					<a href="#Obsessiveness">MĘCZĄ MNIE NATRĘCTWA</a>
-					<a href="#Anxiety">PRZYTŁACZA MNIE LĘK</a>
-					<a href="#SelfEsteem">CHCĘ WZMOCNIĆ SAMOOCENĘ</a>
-				</div>
+				))}
 			</div>
     </Layout>
   )
 }
-
 export default TherapyPage
