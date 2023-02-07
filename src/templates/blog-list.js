099009c4a88648e7ead064f8740f0ad81564c548
Paweł Cybulski <p.cybulski@mohi.to>
2023-02-07 08:36:02 +0100

    blog fix

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -88,7 +88,7 @@ class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
     const { currentPage, numPages } = this.props.pageContext
-    const blogSlug = "/blog/"
+    const blogSlug = "/"
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
     const prevPage =
